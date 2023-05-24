4e0a390e81c73be42cc218e94f247ea2ebca60d6
proesn <69461670+proesn@users.noreply.github.com>
2023-05-24 19:45:48 +0900

    조치사항에 사진 추가 기능 구현
    백엔드 연결 완료.
    사진 저장 및 다시 보기 가능

--- firebase.js
+++ firebase.js
@@ -2,6 +2,7 @@
 import { initializeApp } from "firebase/app";
 // import { getAnalytics } from "firebase/analytics";
 import { getDatabase } from "firebase/database";
+import { getStorage } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -22,9 +23,9 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 // const analytics = getAnalytics(app);
 const database = getDatabase(app);
-
+const storage= getStorage(app);
 export {database};
-
+export {storage}
 /*
 
 import { database } from './firebase';
